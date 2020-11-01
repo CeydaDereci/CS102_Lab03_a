e9c152f05709d05623f897c805a5f27f490174e5
Ceyda Dereci <ceyda.dereci@ug.bilkent.edu.tr>
2020-11-02 00:34:53 +0300

    Part a updated.

--- src/Triangle.java
+++ src/Triangle.java
@@ -9,7 +9,7 @@ public class Triangle extends Shape{
     double getArea() {
         double p;
         double area;
-        p = (a + b + c) / 2;
+        p = (double)(a + b + c) / 2;
         area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
         return area;
     }
